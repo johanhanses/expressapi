e8eac5ceeae16d3ea8126e6ffbd3abf6c650ef23
Johan Hanses <you@example.com>
2019-09-07 20:34:43 +0200

    added a report title route

--- routes/reports.js
+++ routes/reports.js
@@ -25,6 +25,29 @@ async function getReport(week) {
     });
 }
 
+router.get("/week/all", async (req, res) => {
+    // const id = req.params.id;
+    const allWeeks = await getReportTitle();
+    const data = {allWeeks};
+
+    res.status(200).json(data);
+});
+
+async function getReportTitle() {
+    return new Promise((resolve, reject) => {
+        db.all(
+            "SELECT week FROM reports",
+            (err, data) => {
+                if (err) {
+                    reject("Read error: " + err.message);
+                } else {
+                    resolve(data);
+                }
+            }
+        );
+    });
+}
+
 
 
 module.exports = router;
